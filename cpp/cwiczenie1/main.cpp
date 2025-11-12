b57d6b4dc438da289086037ce49c81362250f338
OzC4R20102 <okrawczykiewicz@gmail.com>
2025-11-12 12:12:37 +0000

    studentmachine send

---
+++ cpp/cwiczenie1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int x;
+cout<<"wpisz liczbe";
+cin>>x;
+if(x<2){
+	cout<<"nie jest pierwsza";
+}
+for(int i=2; i*i <=x;i++){
+	i(x%1==0){
+	cout<<"liczba nie jest pierwsza";
+}
+
+}
+	return 0;
+}
